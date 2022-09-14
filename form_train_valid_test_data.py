32ef8a6beb6015b7444ee1927477ad8885f8da3d
Sudarshan <su0@ornl.gov>
2022-09-14 11:56:34 -0400

    Changed 4 to 2 spaces

--- form_train_valid_test_data.py
+++ form_train_valid_test_data.py
@@ -8,30 +8,31 @@ import json
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--video_clip_json_file')
-    args = parser.parse_args()
-    return args
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--video_clip_json_file')
+  args = parser.parse_args()
+  return args
 
 
-def form_data(video_clips):
+def form_data(video_clips):  
+  for clip in video_clips:
+    if clip['rst'] == -1 and clip['rend'] == 1:
+      pass
     
-    for clip in video_clips:
-        if clip['rst'] == -1 and clip['rend'] == 1:
-            
+      
 
 
 def main():
-    args = parse_args()
-    video_clip_json_file = args.video_clip_json_file
-    with open(video_clip_json_file, 'r') as f:
-        video_clips = json.load(f)
-    train_video_clips = clips['train_video_clips']
-    val_video_clips = clips['val_video_clips']
-    test_video_clips = clips['test_video_clips']
-    
+  args = parse_args()
+  video_clip_json_file = args.video_clip_json_file
+  with open(video_clip_json_file, 'r') as f:
+    video_clips = json.load(f)
+  train_video_clips = clips['train_video_clips']
+  val_video_clips = clips['val_video_clips']
+  test_video_clips = clips['test_video_clips']
+  
 
 
 if __name__ == "__main__":
-    main()
+  main()
 
